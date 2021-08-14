7edea48a9f216b3ccb8012b923294486dc9c6186
Shravan Ravi <36645942+ShravanRavi2002@users.noreply.github.com>
2021-08-13 20:21:47 -0500

    Updates

--- static/js/react-syntax-highlighter_languages_refractor_ini.ee3d9265.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonppersonal-portfolio"]=this["webpackJsonppersonal-portfolio"]||[]).push([[60],{658:function(t,n,s){"use strict";function o(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=o,o.displayName="ini",o.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.ee3d9265.chunk.js.map
