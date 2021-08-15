a0da79fad1f32c22d64fb5462388bd66ac6e317b
Shravan Ravi <36645942+ShravanRavi2002@users.noreply.github.com>
2021-08-15 13:15:03 -0500

    updated portfolio section design

--- src/containers/blogs/Blogs.js
+++ src/containers/blogs/Blogs.js
@@ -12,48 +12,51 @@ export default function Blogs() {
   }
   var name = "Shravan"
   return (
-    <Container >
-      <Fade bottom duration={1000} distance="20px">
-              <div className="d-flex px-3" style={{paddingBottom: "20px"}}>
-                  <div>
-                      <div className="icon icon-lg icon-shape bg-gradient-white shadow rounded-circle text-info">
-                      <i className="ni ni-books text-info" />
-                      </div>
-                  </div>
-                  <div className="pl-4">
-                  <h4 className="display-3 text-info">Publications</h4>
-                  </div>
-            </div>
-            <Row style={{justifyContent: "center", margin: "center"}}>
-              <div className="main" id="blogs">
-                <div className="blog-header">
-                </div>
-                <div className="blog-main-div">
-                  <div className="blog-text-div">
-                    {blogSection.blogs.map((blog, i) => {
-                      return (
-                        <BlogCard
-                          key={i}
-                          // isDark={isDark}
-                          blog={{
-                            url: blog.url,
-                            image: blog.image,
-                            title: blog.title,
-                            description: blog.description,
-                            conference: blog.conference,
-                            submission: blog.submission,
-                            finished: blog.url != null
-                          }}
-                        />
-                      );
-                    })}
-                  </div>
-                </div>
+    <div style={{backgroundColor: "#11CDEF"}}>
+            <Container >
+        <Fade bottom duration={1000} distance="20px">
+                <div className="d-flex px-3" style={{paddingBottom: "20px"}}>
+                    <div>
+                        <div className="icon icon-lg icon-shape bg-gradient-white shadow rounded-circle text-info">
+                        <i className="ni ni-books text-info" />
+                        </div>
+                    </div>
+                    <div className="pl-4">
+                    <h4 className="display-3" style={{color: "white"}}>Publications</h4>
+                    </div>
               </div>
-            </Row>
-            
-          </Fade>
-    </Container>
+              <Row style={{justifyContent: "center", margin: "center"}}>
+                <div className="main" id="blogs">
+                  <div className="blog-header">
+                  </div>
+                  <div className="blog-main-div">
+                    <div className="blog-text-div">
+                      {blogSection.blogs.map((blog, i) => {
+                        return (
+                          <BlogCard
+                            key={i}
+                            // isDark={isDark}
+                            blog={{
+                              url: blog.url,
+                              image: blog.image,
+                              title: blog.title,
+                              description: blog.description,
+                              conference: blog.conference,
+                              submission: blog.submission,
+                              finished: blog.url != null
+                            }}
+                          />
+                        );
+                      })}
+                    </div>
+                  </div>
+                </div>
+              </Row>
+              
+            </Fade>
+      </Container>
+    </div>
+    
     
   );
 }
