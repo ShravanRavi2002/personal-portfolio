f9a1975dfed3746abe17975b1199861ed061bd8b
Shravan Ravi <36645942+ShravanRavi2002@users.noreply.github.com>
2021-08-17 22:21:14 -0500

    updated publications section to have different backgrounds

--- src/components/blogCard/BlogCard.js
+++ src/components/blogCard/BlogCard.js
@@ -3,6 +3,7 @@ import "./BlogCard.scss";
 import {blogSection} from "../../portfolio";
 import GreetingLottie from "../../components/DisplayLottie";
 import construction from "../../assets/lottie/construction.json"
+import {Fade} from "react-reveal";
 
 function BoldedText( text, shouldBeBold, isDark ) {
   const textArray = text.split(shouldBeBold);
@@ -24,43 +25,46 @@ function BoldedText( text, shouldBeBold, isDark ) {
 export default function BlogCard({blog, isDark}) {
   isDark=false;
   function openUrlInNewTab(url) {
-    if (url !== undefined) {
+    if (url !== undefined && url !== null ) {
       var win = window.open(url, "_blank");
       win.focus();
     }
   }
 
   return (
-    <div onClick={() => openUrlInNewTab(blog.url)}>
-      <div className={isDark ? "blog-container dark-mode" : "blog-container"}>
-        <a
-          className={
-            isDark ? "dark-mode blog-card blog-card-shadow" : "blog-card"
-          }
-        >
-            <h3 className={isDark ? "small-dark blog-title" : "blog-title"}>
-              {blog.title}
-            </h3>
-          {console.log(isDark)}
-          {BoldedText(blog.description, blogSection.name, isDark)}
-          {blog.finished ? null :  
-          <div className="blog-construction">
-              <GreetingLottie animationData={construction}  style={{maxWidth: "100%", maxHeight: "100%"}}/>
+    <Fade left duration={1000} distance="40px">
+              <div onClick={() => openUrlInNewTab(blog.url)}>
+          <div className={isDark ? "blog-container dark-mode" : "blog-container"}>
+            <a
+              className={
+                isDark ? "dark-mode blog-card blog-card-shadow" : "blog-card"
+              }
+            >
+                <h3 className={isDark ? "small-dark blog-title" : "blog-title"}>
+                  {blog.title}
+                </h3>
+              {console.log(isDark)}
+              {BoldedText(blog.description, blogSection.name, isDark)}
+              {blog.finished ? null :  
+              <div className="blog-construction">
+                  <GreetingLottie animationData={construction}  style={{maxWidth: "100%", maxHeight: "100%"}}/>
+              </div>
+              
+              }
+              <hr style={{width: "90%", justifyContent: "center", margin: "auto"}}></hr>
+              <h3 className="blog-conference">{blog.conference}</h3>
+              {
+                blog.finished ? <h3 className="blog-submission">{`Submitted on: ${blog.submission}`}</h3> : <h3 className="blog-submission">{`Submitting on: ${blog.submission}`}</h3>    
+              }          
+              
+              
+              <div className="go-corner">
+                <div className="go-arrow">→</div>
+              </div>
+            </a>
           </div>
-          
-          }
-          <hr style={{width: "90%", justifyContent: "center", margin: "auto"}}></hr>
-          <h3 className="blog-conference">{blog.conference}</h3>
-          {
-            blog.finished ? <h3 className="blog-submission">{`Submitted on: ${blog.submission}`}</h3> : <h3 className="blog-submission">{`Submitting on: ${blog.submission}`}</h3>    
-          }          
-          
-          
-          <div className="go-corner">
-            <div className="go-arrow">→</div>
-          </div>
-        </a>
-      </div>
-    </div>
+        </div>
+    </Fade>
+    
   );
 }
