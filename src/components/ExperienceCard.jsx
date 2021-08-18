c0423e382f8c2e2c4e57aff64821af794373a777
Shravan Ravi <36645942+ShravanRavi2002@users.noreply.github.com>
2021-08-17 22:19:36 -0500

    updated experience card ux

--- src/components/ExperienceCard.jsx
+++ src/components/ExperienceCard.jsx
@@ -34,24 +34,24 @@ const ExperienceCard = ({data}) => {
 
     return ( 
         <Col lg="4">
-        <Fade left duration={1000} distance="40px">
+        <Fade left duration={1000} distance="20px">
             <Card style={{position: "relative", flexBasis: "auto", top: "100%", marginBottom: "2rem"}} className="shadow-lg--hover shadow border-0 text-center rounded">
-                <CardHeader style={{background: data.color === undefined ? rgb(colorArrays)  : colorArrays}} >
+                <CardHeader style={{background: data.color === undefined ? rgb(colorArrays)  : colorArrays, marginBottom: 0}} >
                     <h5 className="text-white">{data.company}</h5>
                 </CardHeader>
                 <CardBody className="py-5">
-                    <a href={data.link} target="_blank" rel="noopener noreferrer">
-                        <img ref={imgRef} className=" bg-white rounded-circle mb-3 img-center img-fluid shadow-lg " src={data.companylogo} style={{ width: "100px", height: "100px" }} onLoad={() => getColorArrays()} alt=""/>
+                    <a href={data.link} target="_blank" rel="noopener noreferrer" style={{width: "max-content", height: "max-content"}}>
+                        <img ref={imgRef} href={data.link}  className="bg rounded-circle mb-3 img-center img-fluid shadow-lg--hover " src={data.companylogo} style={{ width: "110px", height: "110px" }} onLoad={() => getColorArrays()} alt=""/>
                     </a>
                     
                     <CardTitle tag="h5">{data.role}</CardTitle>
                     <CardSubtitle className="text-black" style={{paddingBottom: 15}}>{data.date}</CardSubtitle>
-                    {moreInfo ? null : <Button onClick={() => changeInfo(!moreInfo)}>More Info</Button>}
-                    
+                    {moreInfo ? null : <Button onClick={() => changeInfo(!moreInfo)} style={{marginTop: "10px", marginBottom: 0}}>More Info</Button>}
+                     
                     { 
                     moreInfo ? 
                     <div>
-                                                    <hr style={{width: "80%", justifyContent: "center", margin: "auto"}}></hr>
+                        <hr style={{width: "80%", justifyContent: "center", margin: "auto"}}></hr>
                         <CardText className="description my-3 text-center text-black" style={{color: "black", justifyContent: "center", margin: "auto",}}>
                             {data.desc}
                         </CardText>
--- src/containers/Experience.jsx
+++ src/containers/Experience.jsx
@@ -11,13 +11,12 @@ import ExperienceCard from "../components/ExperienceCard";
 import "./Education.scss"
 const Experience = () => {
     return ( 
-        <section className="section section-lg" style={{paddingTop: 0, paddingBottom: 0}}>
+        <section className="section section-lg" style={{paddingTop: 0, paddingBottom: 0}} id="experience">
             <Container>
-              <Fade left duration={1000} distance="40px">
-              <div className="d-flex p-4">
+            <div className="d-flex p-4">
                     <div>
                         <div className="icon icon-lg icon-shape bg-gradient-white shadow rounded-circle text-info">
-                        <i className="ni ni-briefcase-24 text-info" />
+                        <i className="fa fa-briefcase text-info" />
                         </div>
                     </div>
                     <div className="pl-4">
@@ -33,7 +32,6 @@ const Experience = () => {
                         }
                     </div>
                 </Row>
-              </Fade>
             </Container>
           </section>
      );
